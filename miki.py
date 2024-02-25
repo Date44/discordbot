b0e65987a151453696fe99781a56a7fabe0868b0
Date44 <mgura1833@gmail.com>
2024-02-25 11:11:11 +0100

    push

--- miki.py
+++ miki.py
@@ -685,14 +685,13 @@ async def printer(channel):
     cur.execute("SELECT * FROM Users WHERE ban_timeout != 0")
     all = cur.fetchall()
     for i in all:
-        print(i[3])
         current_time_str = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
         time_obj = datetime.datetime.strptime(i[3], '%H:%M:%S %d-%m-%Y')
         current_time_obj = datetime.datetime.strptime(current_time_str, '%H:%M:%S %d-%m-%Y')
         if current_time_obj >= time_obj:
-            print(str(time_obj) + " =< " + current_time_str)
+            print(current_time_obj + " >= " + str(time_obj))
         elif current_time_obj < time_obj:
-            print(str(time_obj) + " > " + current_time_str)
+            print(current_time_obj + " < " + str(time_obj))
 
 
 # 1. Во время запуска бота
