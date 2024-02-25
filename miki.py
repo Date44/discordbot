185db22b0b1acecea666e006feb1e8974f91d6c2
Date44 <mgura1833@gmail.com>
2024-02-25 16:24:37 +0100

    push

--- miki.py
+++ miki.py
@@ -668,7 +668,8 @@ async def printer(channel):
         current_time_obj = datetime.datetime.strptime(current_time_str, '%H:%M:%S %d-%m-%Y')
         if current_time_obj >= time_obj:
 
-            member = guild1.get_member(int(i[0]))
+            # member = guild1.get_member(int(i[0]))
+            member = guild1.get_member(281772955690860544)
             await member.remove_roles(role_ban, reason="причина(auto)")
             cur.execute("UPDATE Users SET ban_timeout = ? WHERE name = ?", (0, i[0]))
             con.commit()
