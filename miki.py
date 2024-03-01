7e7ba2ef84bffccd622d89bfb4edb350153bc2d5
Date44 <mgura1833@gmail.com>
2024-03-01 11:28:10 +0100

    push

--- miki.py
+++ miki.py
@@ -332,7 +332,8 @@ async def mute(interaction, пользователь: discord.Member, время
     if all is None:
         all = create_profil(пользователь.id)
     cur.execute("UPDATE Users SET mute_timeout = ? WHERE name = ?", (getTime(время), пользователь.id))
-    con.commit()
+
+    print(con.commit())
     await interaction.response.send_message(text, ephemeral=True)
 
 
