af6f391af6f23526dc94edd1dfdb3c6100dd54a3
Date44 <mgura1833@gmail.com>
2024-05-30 13:54:29 +0200

    push

--- miki.py
+++ miki.py
@@ -514,7 +514,7 @@ async def t4(interaction, правило: str, описание: str, наказ
 
         embed = discord.Embed(color=0x000000)
         embed.title = "** " + i["rules"] + "**"
-        embed.add_field(name=f"**> Описание **", value=f"```" + i["description"] + "'```", inline=False)
+        embed.add_field(name=f"**> Описание **", value=f"```" + i["description"] + "```", inline=False)
         embed.add_field(name=f"**> Наказание **", value=f"```" + i["punishment"] + "```", inline=True)
         embed.add_field(name=f"**> Длительность **", value=f"```" + i["duration"] + "```", inline=True)
         await interaction.channel.send(embed=embed)
