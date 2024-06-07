31e40dc1dedaa4156397c9acf96a4a38d4325dfc
Date44 <mgura1833@gmail.com>
2024-06-07 21:58:01 +0200

    push

--- miki.py
+++ miki.py
@@ -166,11 +166,11 @@ async def create_rules(message):
 
 
 @tree.context_menu(name="edit", guild=discord.Object(id=guild_id))
-async def edit_rules(interaction: discord.Interaction, member: discord.Member):
-    if member == Bot.user:
-        await interaction.response.send_message(interaction.message)
+async def edit_rules(interaction: discord.Interaction, message: discord.Message):
+    if interaction.user == Bot.user:
+        await interaction.response.send_message(message)
     else:
-        await interaction.channel.send("123", suppress_embeds=True)
+        await interaction.response.send_message("123", suppress_embeds=True)
     # text = message.content
     # text = text.split("\n")
     # line = (text[0].replace(f"https://discord.com/channels/{guild_id}/", "")
