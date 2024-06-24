7ef9620f1c627a50f429f1dc28d871eaeaeba0f3
Date44 <mgura1833@gmail.com>
2024-06-24 16:45:50 +0200

    push

--- miki.py
+++ miki.py
@@ -192,7 +192,7 @@ async def edit_rules(interaction: discord.Interaction, message: discord.Message)
 
 
 async def test(message):
-    await message.channel.send(message.content)
+    await message.channel.send(message)
 
 
 async def restart(message):
