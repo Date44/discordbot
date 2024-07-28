789c34fbb094ef02e2b1ad461e68cf9e3f431584
Date44 <mgura1833@gmail.com>
2024-07-28 22:03:36 +0200

    push

--- miki.py
+++ miki.py
@@ -570,6 +570,7 @@ async def check(interaction: discord.Interaction, пользователь: disc
         cur.execute("SELECT * FROM History WHERE name == ?", (пользователь.id,))
         all_entries = cur.fetchall()
         if len(all_entries) > 0:
+            s1 += "## История наказаний\n`тип/время` `причина` `модератор`"
             for i in all_entries:
                 if i[2] == 0:
                     s1 += "<:pred:1267205995231187056> "
